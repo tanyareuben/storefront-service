04894852c699b14dad36755eecfb7f5a8a7c25f9
tanyareuben <tanyareuben@gmail.com>
2023-11-24 19:35:42 -0800

    Added food items for each category
    removed poultry category

--- src/main/java/com/sjsu/storefront/common/ProductCategory.java
+++ src/main/java/com/sjsu/storefront/common/ProductCategory.java
@@ -1,12 +1,11 @@
 package com.sjsu.storefront.common;
 
 public enum ProductCategory {
-	VEGITABLE,
+	VEGETABLE,
 	FRUIT,
-	POULTRY,
 	MEAT,
 	SEAFOOD,
 	GRAIN,
-	DIARY,
+	DAIRY,
 	PANTRY_STAPLES
 }
